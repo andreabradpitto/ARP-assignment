732f6b14d67029b2b58fcc753a0f9d6a081d1310
Andrea Pitto <andreabradpitto@gmail.com>
2021-07-17 13:23:53 +0200

    Minor Terminal UI fix

--- src/S.c
+++ src/S.c
@@ -89,9 +89,9 @@ int main(int argc, char *argv[])
 	setenv("command3", command3, 1);
 
 	// launch a new terminal (Input Terminal) that allows the user to send signals to the running processes
-	int exit_status = system("gnome-terminal -- sh -c \"echo $welcome1; echo $welcome0; echo $welcome2; "
-							 "echo $welcome0; echo $command1; echo $command2; echo $command3; echo $welcome0; "
-							 "echo $welcome3; echo $welcome0; exec bash\"");
+	int exit_status = system("gnome-terminal -- sh -c \"echo $welcome0; echo $welcome1; echo $welcome0; "
+							 " echo $welcome2; echo $welcome0; echo $command1; echo $command2; echo $command3; "
+							 "echo $welcome0; echo $welcome3; echo $welcome0; exec bash\"");
 
 	printf("S: my PID is %d\n", Spid);
 
